7fac078cc7618d09a399b1e0d1abf3b1c17156d4
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-10 18:48:39 -0400

    foundling

---
+++ src/data.jsx
@@ -0,0 +1,46 @@
+import { FaXTwitter, FaLinkedin, FaPizzaSlice } from "react-icons/fa6"
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 5,
+    url: "/profile",
+    text: "profile",
+  },
+]
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.x.com/matthewcordaro",
+    icon: <FaXTwitter />,
+  },
+  {
+    id: 2,
+    url: "https://www.linkedin.com/in/matthewcordaro/",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 3,
+    url: "https://www.matthewcordaro.com/",
+    icon: <FaPizzaSlice />,
+  },
+]
